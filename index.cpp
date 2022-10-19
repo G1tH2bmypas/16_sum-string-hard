aa0041e12d35eeae7d38270cf60bea5e5f89749f
Tekyto <Tekyto2014@gmail.com>
2022-10-19 22:03:46 +0300

    variable

--- index.cpp
+++ index.cpp
@@ -3,5 +3,30 @@
 #include <cmath>
 
 int sumString(const std::string& str) {
-	return 0;
+	int num = 0;
+	int negative = false;
+	int variable = 0;
+
+	for (char ch: str) {
+		variable = 0;
+
+		if (ch == '-') {
+			negative = true;
+			continue;
+		}
+
+		int current = ch - 48;
+
+		if (current <= 9 && current >= 0) {
+			variable = current;
+		}
+
+		if (negative) {
+			variable *= -1;
+			negative = false;
+		}
+
+		num += variable;
+	}
+	return num;
 }
